ba52c88f2ccd404d86247181d973377398639abc
Pranay Narain <pranaynarain05@gmail.com>
2026-05-29 21:34:32 +0530

    fix: use CLIENT_URL env for CORS

--- server/src/app.js
+++ server/src/app.js
@@ -13,7 +13,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: process.env.CLIENT_URL || 'http://localhost:5173' ||"https://digital-heal.netlify.app",
   credentials: true
 }));
 app.use(cookieParser());
